e89366e6136519a29ea7b7cd0cdd662348d24c98
Yuya Tsuneoka <tsuneoka@mizuuchi.lab.tuat.ac.jp>
2016-07-16 16:17:35 +0900

    SmokingRobotクラスを作成

--- smoking_liquid.py
+++ smoking_liquid.py
@@ -19,17 +19,62 @@ class Timer():
         return time.time() - self._starttime
 
 
+class SmokingRobot():
+    def __init__(self, port, startAngle=0.0, endAngle=1.0, t_breathe=1.0, t_wait=1.0, t_emit=1.0):
+        self.ser = serial.Serial(port, 115200, timeout=0.1)
+        self.nowAngle = startAngle
+        self.startAngle = startAngle
+        self.endAngle = endAngle
+        self.setSmokeTime(t_breathe, t_wait, t_emit)
+        print("Hello! I'm a Smoking Robot.")
+
+    def setSmokeTime(self, t_breathe, t_wait, t_emit):
+        self.t_breathe = t_breathe
+        self.t_wait = t_wait
+        self.t_emit = t_emit
+
+    def smoke(self):
+        print("smoke start")
+        self.breathe(self.t_breathe)
+        time.sleep(self.t_wait)
+        self.emit(self.t_emit)
+        print("smoke end")
+
+    def breathe(self, t):
+        print("breathe start")
+        self._moveLinear(self.endAngle, t)
+        print("breathe end")
+
+    def emit(self, t):
+        print("emit start")
+        self._moveLinear(self.startAngle, t)
+        print("emit end")
+
+    def _move(self, targetAngle):
+        self.ser.write(chr(int(targetAngle * 255)))
+        self.nowAngle = targetAngle
+        print(int(targetAngle * 255))
+        time.sleep(0.05)
+
+    def _moveLinear(self, targetAngle, targetTime):
+        startAngle = self.nowAngle
+        timer = Timer(targetTime)
+        func = lambda t: (startAngle + t / targetTime * (targetAngle - startAngle))
+        print("linear motor control start")
+        timer.start()
+        while (not timer.endJudge()):
+            self._move(func(timer.interval()))
+        print("linear motor control end")
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+    r = SmokingRobot('/dev/ttyACM0')
     while True:
-        value = raw_input("motor value 0-255 >>> ")
-        if (value.isdigit()):
-            ser.write(chr(int(value)))
+        value = raw_input("input s to smoke >>> ")
+        if (value == "s"):
+            r.smoke()
         elif (value == "q"):
             print("終了します")
             break
         else:
             print("error: 入力が数字ではありません")
-    ser.close()
 
